ac9c606b1832a163745c28a2a36f0b2cba35235e
Aleksei <vuraddo@yahoo.com>
2024-08-28 20:21:54 +0100

    Update plugins; Fix dap; Update lualine layout

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,24 +1,54 @@
 require("lualine").setup({
     options = {
         theme = "rose-pine",
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
     },
     sections = {
-        lualine_a = { { "mode", fmt = function(str) return str:sub(1, 1) end } },
-        lualine_b = { "branch" },
-        lualine_c = { "require('lsp-progress').progress()" },
+        lualine_a = {
+            { "mode",   fmt = function(str) return str:sub(1, 1) end },
+            { "branch", icons_enabled = false },
+        },
+        lualine_b = {
+            {
+                "filename",
+                path = 1,
+                file_status = true,
+                symbols = {
+                    modified = '~',
+                    readonly = '-',
+                    unnamed = '',
+                    newfile = '(new)',
+                },
+            },
+        },
+        lualine_c = {
+            {
+                'diff',
+                colored = true,                                           -- Displays a colored diff status if set to true
+                symbols = { added = '+', modified = '~', removed = '-' }, -- Changes the symbols used by the diff.
+            },
+        },
         lualine_x = {
             {
                 "diagnostics",
-                -- Displays diagnostics for the defined severity types
                 sections = { "error", "warn" },
                 symbols = { error = 'E', warn = 'W' },
-                colored = true, -- Displays diagnostics status in color if set to true.
-                update_in_insert = false, -- Update diagnostics in insert mode.
-                always_visible = false, -- Show diagnostics even if there are none.
+                colored = true,
+                update_in_insert = false,
+                always_visible = false,
             },
-            "filetype",
         },
-        lualine_y = { "progress" },
+        lualine_y = {
+            "require('lsp-progress').progress()",
+            {
+                "filetype",
+                cond = function()
+                    local progress = require('lsp-progress').progress()
+                    return progress == nil or progress == ""
+                end,
+            },
+        },
         lualine_z = { "location" },
     },
 })
